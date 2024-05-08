e4a60d765a6d089b6ba78cec453c0042fd9a43b7
Ferry1231 <lfy1231@mail.ustc.edu.cn>
2024-05-08 14:36:13 +0800

    Initial commit

---
+++ LLMs/spark.py
@@ -0,0 +1,28 @@
+from sparkai.llm.llm import ChatSparkLLM, ChunkPrintHandler
+from sparkai.core.messages import ChatMessage
+
+#星火认知大模型v3.5的URL值，其他版本大模型URL值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
+SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
+#星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
+SPARKAI_APP_ID = '2b68f247'
+SPARKAI_API_SECRET = 'ZWFhMWM2YzRlODNlNmMyOWQxOTZhMjI1'
+SPARKAI_API_KEY = 'fc2592b5967a150ee7237269aacf8b34'
+#星火认知大模型v3.5的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
+SPARKAI_DOMAIN = 'generalv3.5'
+
+if __name__ == '__main__':
+    spark = ChatSparkLLM(
+        spark_api_url=SPARKAI_URL,
+        spark_app_id=SPARKAI_APP_ID,
+        spark_api_key=SPARKAI_API_KEY,
+        spark_api_secret=SPARKAI_API_SECRET,
+        spark_llm_domain=SPARKAI_DOMAIN,
+        streaming=False,
+    )
+    messages = [ChatMessage(
+        role="user",
+        content='你好呀'
+    )]
+    handler = ChunkPrintHandler()
+    a = spark.generate([messages], callbacks=[handler])
+    print(a)
