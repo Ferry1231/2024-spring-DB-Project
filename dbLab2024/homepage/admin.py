38b6125e7e3246716bd507c78ec341dc18a01180
Ferry1231 <lfy1231@mail.ustc.edu.cn>
2024-05-30 22:04:47 +0800

    update

--- dbLab2024/homepage/admin.py
+++ dbLab2024/homepage/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Students,Mentors,Research,Courses,Grades,selectedMaterials,Materials,Subject
 
-# Register your models here.
+admin.site.register(Students)
+admin.site.register(Mentors)
+admin.site.register(Research)
+admin.site.register(Courses)
+admin.site.register(Grades)
+admin.site.register(selectedMaterials)
+admin.site.register(Materials)
+admin.site.register(Subject)
--- dbLab2024/homepage/models.py
+++ dbLab2024/homepage/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Students(models.Model):
+    stu_id = models.CharField(max_length=20,help_text="enter student id",primary_key=True)
+    stu_name = models.CharField(max_length=10,help_text="enter student name")
+    stu_gender = models.CharField(max_length=10,help_text="enter student gender")
+    stu_age = models.IntegerField(help_text="enter student age")
+    mentor_id = models.ForeignKey('Mentors',to_field='mentor_id',on_delete=models.RESTRICT,null=True)
+    res_name = models.ForeignKey('Research',to_field='res_name',on_delete=models.RESTRICT,null=True)
+    
+    def __str__(self):
+        return self.stu_name
+    
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.stu_id)])
+    
+class Mentors(models.Model):
+    mentor_id = models.CharField(max_length=20,help_text="enter mentor id",primary_key=True)
+    mentor_name = models.CharField(max_length=10,help_text="enter mentor name")
+    mentor_gender = models.CharField(max_length=10,help_text="enter mentor gender")
+    res_name = models.ForeignKey('Research',to_field='res_name',on_delete=models.RESTRICT,null=True)
+
+    def __str__(self):
+        return self.mentor_name
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.mentor_id)])
+    
+class Research(models.Model):
+    res_name = models.CharField(max_length=20,help_text="enter res name",primary_key=True)
+    res_summary = models.TextField(help_text="enter summary of the research")
+    res_subject = models.ForeignKey('Subject',to_field='subject_id',on_delete=models.RESTRICT,null=True)
+
+    def __str__(self):
+        return self.res_name
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.res_name)])
+
+class Courses(models.Model):
+    cous_subject = models.ForeignKey('Subject',to_field='subject_id',on_delete=models.RESTRICT,null=True)
+    cous_id = models.CharField(max_length=20,help_text="enter course id",primary_key=True)
+    cous_summary = models.TextField(help_text="enter summary of the course")
+    cous_name = models.CharField(max_length=10,help_text="enter course name")
+
+    def __str__(self):
+        return self.cous_name
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.cous_id)])
+    
+class Grades(models.Model):
+    cous_id = models.ForeignKey('Courses',to_field='cous_id',on_delete=models.RESTRICT,null=True)
+    stu_id = models.ForeignKey('Students',to_field='stu_id',on_delete=models.RESTRICT,null=True)
+    score = models.FloatField(help_text="enter the score")
+
+    class Meta:
+        unique_together = (('cous_id', 'stu_id',),)
+
+    def __str__(self):
+        return self.cous_id + " " + str(self.score)
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.cous_id + " " + str(self.score))])
+    
+class selectedMaterials(models.Model):
+    matr_id = models.ForeignKey('Materials',to_field='matr_id',on_delete=models.RESTRICT,null=True)
+    stu_id = models.ForeignKey('Students',to_field='stu_id',on_delete=models.RESTRICT,null=True)
+    
+    class Meta:
+        unique_together = (('matr_id', 'stu_id',),)
+
+    def __str__(self):
+        return self.matr_id + " " + self.stu_id
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.matr_id+self.stu_id)])
+    
+class Materials(models.Model):
+    matr_id =  models.CharField(max_length=20,help_text="inter materials id",primary_key=True)
+    matr_summary = models.TextField(help_text="enter summary of the material")
+    cous_id = models.ForeignKey('Courses',to_field='cous_id',on_delete=models.RESTRICT,null=True)
+
+    def __str__(self):
+        return self.matr_id
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.matr_id)])
+    
+class Subject(models.Model):
+    subject_id = models.CharField(max_length=20,help_text="enter the subject id",primary_key=True)
+    subject_name = models.CharField(max_length=10,help_text="enter the subject name")
+    subject_summary = models.TextField(help_text="enter summary of the subject")
+    
+    def __str__(self):
+            return self.subject_name
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.subject_id)])
